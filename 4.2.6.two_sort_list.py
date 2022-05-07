1ceee70476eeee776f5cda22b68c6676b0e543a5
Evgenii <evgenii889111@gmail.com>
2022-05-07 17:34:33 +0300

    del while 4.2.6

--- 4.2.6.two_sort_list.py
+++ 4.2.6.two_sort_list.py
@@ -30,21 +30,5 @@ if __name__ == '__main__':
     n, m = map(int, input().split())
     list1 = [int(i) for i in input().split()]
     list2 = [int(i) for i in input().split()]
-    result = []
-    p1 = 0
-    p2 = 0
-    while p1 < n and p2 < m:
-        if list1[p1] <= list2[p2]:
-            result.append(list1[p1])
-            p1 += 1
-        else:
-            result.append(list2[p2])
-            p2 += 1
-    if p1 < len(list1):
-        result += list1[p1:]
-
-    if p2 < len(list2):
-        result += list2[p2:]
-    print(*result)
     print(*merge_two_sotr_list(list1, list2))
 
