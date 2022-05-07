963f9c2e8178b93ace117a98f5922fd446154f1f
Evgenii <evgenii889111@gmail.com>
2022-05-07 17:12:58 +0300

    create function merge_two_sotr_list 4.2.6

--- 4.2.6.two_sort_list.py
+++ 4.2.6.two_sort_list.py
@@ -9,10 +9,25 @@
 # P.S: пользоваться встроенной сортировкой запрещено
 # Примечание: для вывода результирующего списка вы можете использовать следующую конструкцию
 # print(*result) # где result - итоговый список
+def merge_two_sotr_list(list1: list, list2: list) -> list:
+    result = []
+    p1 = 0
+    p2 = 0
+    while p1 < n and p2 < m:
+        if list1[p1] <= list2[p2]:
+            result.append(list1[p1])
+            p1 += 1
+        else:
+            result.append(list2[p2])
+            p2 += 1
+    if p1 < len(list1):
+        result += list1[p1:]
+    if p2 < len(list2):
+        result += list2[p2:]
+    return result
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-
     list1 = [int(i) for i in input().split()]
     list2 = [int(i) for i in input().split()]
     result = []
@@ -31,3 +46,4 @@ if __name__ == '__main__':
     if p2 < len(list2):
         result += list2[p2:]
     print(*result)
+
