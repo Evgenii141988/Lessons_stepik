c89b30a863152aa2c819cba78158547183036107
Evgenii <evgenii889111@gmail.com>
2022-05-08 19:34:50 +0300

    add if __name__ 4.4.2

--- indi_programming_egoroff_chanel/4.4.2.py
+++ indi_programming_egoroff_chanel/4.4.2.py
@@ -1,14 +1,15 @@
 # Программа получает на вход натуральное число N.
 # Нужно найти сумму его делителей.
-n = int(input())
-total = 0
-i = 1
-while i * i <= n:
-    if n % i == 0:
-        total += i
-        if total == n == 1:
-            break
-        if i != n // 2:
-            total += n // i
-    i += 1
-print(total)
+if __name__ == '__main__':
+    n = int(input())
+    total = 0
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            total += i
+            if total == n == 1:
+                break
+            if i != n // 2:
+                total += n // i
+        i += 1
+    print(total)
