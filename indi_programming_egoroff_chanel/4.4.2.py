4e3f41894d1067822ea808b89c7a3423312b8177
Evgenii <evgenii889111@gmail.com>
2022-05-08 19:39:50 +0300

    add annataion function get_sum_del 4.4.2

--- indi_programming_egoroff_chanel/4.4.2.py
+++ indi_programming_egoroff_chanel/4.4.2.py
@@ -1,6 +1,6 @@
 # Программа получает на вход натуральное число N.
 # Нужно найти сумму его делителей.
-def get_sum_del(n):
+def get_sum_del(n: int) -> int:
     total = 0
     i = 1
     while i * i <= n:
@@ -16,15 +16,4 @@ def get_sum_del(n):
 
 if __name__ == '__main__':
     n = int(input())
-    total = 0
-    i = 1
-    while i * i <= n:
-        if n % i == 0:
-            total += i
-            if total == n == 1:
-                break
-            if i != n // 2:
-                total += n // i
-        i += 1
-    print(total)
     print(get_sum_del(n))
