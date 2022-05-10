b5c2a84949775076edbccad1e83e6a3801acdfe9
Evgenii <evgenii889111@gmail.com>
2022-05-10 22:34:39 +0300

    creat function get_nod() 4.5.1

--- indi_programming_egoroff_chanel/4.5.1.algoritm_evklida.py
+++ indi_programming_egoroff_chanel/4.5.1.algoritm_evklida.py
@@ -1,5 +1,13 @@
 # Даны два натуральных числа A и B. Требуется найти их наибольший общий делитель (НОД) методом вычитания
 
+def get_nod(a, b):
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
+
 
 if __name__ == '__main__':
     a, b = [int(i) for i in input().split()]
