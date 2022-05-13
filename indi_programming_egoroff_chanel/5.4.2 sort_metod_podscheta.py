b2772ca6b02a2e365f73f7a877ddbca90798c1a9
Evgenii <evgenii889111@gmail.com>
2022-05-13 19:35:32 +0300

    add if __name__ 5.4.2

--- indi_programming_egoroff_chanel/5.4.2 sort_metod_podscheta.py
+++ indi_programming_egoroff_chanel/5.4.2 sort_metod_podscheta.py
@@ -4,14 +4,13 @@
 # Программа получает на вход число n - количество элементов в списке, затем сами элементы списка
 # Вам необходимо вывести отсортированный список
 # P.S. не пользуйтесь встроенной функцией sorted или методом sort
-
-n = int(input())
-numbers = [int(i) for i in input().split()]
-sort_numbers = [0] * 201
-for num in numbers:
-    sort_numbers[num + 100] += 1
-# print(sort_numbers)
-for i in range(len(sort_numbers)):
-    if sort_numbers[i] > 0:
-        for _ in range(sort_numbers[i]):
-            print(i - 100, end=' ')
+if __name__ == '__main__':
+    n = int(input())
+    numbers = [int(i) for i in input().split()]
+    sort_numbers = [0] * 201
+    for num in numbers:
+        sort_numbers[num + 100] += 1
+    for i in range(len(sort_numbers)):
+        if sort_numbers[i] > 0:
+            for _ in range(sort_numbers[i]):
+                print(i - 100, end=' ')
