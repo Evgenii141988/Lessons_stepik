6f8e3491735fa261f4b3dea56088bfce6166e342
Evgenii <evgenii889111@gmail.com>
2022-05-16 19:52:53 +0300

    create try/finally lesson6_step9

--- test_automation_Selenium_Python/lesson6_step9.py
+++ test_automation_Selenium_Python/lesson6_step9.py
@@ -6,5 +6,10 @@ if __name__ == '__main__':
     link = 'http://suninjuly.github.io/registration1.html'
     brawser = webdriver.Chrome()
     brawser.get(link)
-
-
+    try:
+        pass
+    except Exception as ex:
+        print(ex)
+    finally:
+        time.sleep(10)
+        brawser.quit()
