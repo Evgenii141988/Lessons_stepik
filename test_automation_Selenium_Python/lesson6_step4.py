1ac2766bd8298837a9ecd31934bdcfdac9cb4748
Evgenii <evgenii889111@gmail.com>
2022-05-16 14:51:45 +0300

    change all find_element() task lesson6_step4

--- test_automation_Selenium_Python/lesson6_step4.py
+++ test_automation_Selenium_Python/lesson6_step4.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 link = "http://suninjuly.github.io/simple_form_find_task.html"
@@ -7,15 +8,15 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element_by_tag_name(value1)
+    input1 = browser.find_element(by=By.TAG_NAME, value=value1)
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_name(value2)
+    input2 = browser.find_element(by=By.NAME, value=value2)
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name(value3)
+    input3 = browser.find_element(by=By.CLASS_NAME, value=value3)
     input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_id(value4)
+    input4 = browser.find_element(by=By.ID, value=value4)
     input4.send_keys("Russia")
-    button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element(by=By.CSS_SELECTOR, value="button.btn")
     button.click()
 
 finally:
