eadfe09e104c3c693aab962fab27698765363067
Evgenii <evgenii889111@gmail.com>
2022-05-16 20:02:42 +0300

    change name: input1 - input_first_name; input2 - input_second_name lesson6_step9

--- test_automation_Selenium_Python/lesson6_step9.py
+++ test_automation_Selenium_Python/lesson6_step9.py
@@ -7,10 +7,11 @@ if __name__ == '__main__':
     brawser = webdriver.Chrome()
     brawser.get(link)
     try:
-        input1 = brawser.find_element(By.CLASS_NAME, 'first')
-        input1.send_keys('Ivan')
-        input2 = brawser.find_element(By.CLASS_NAME, 'second')
-        input2.send_keys('Ivanov')
+        input_first_name = brawser.find_element(By.CLASS_NAME, 'first')
+        input_first_name.send_keys('Ivan')
+        input_last_name = brawser.find_element(By.CLASS_NAME, 'second')
+        input_last_name.send_keys('Ivanov')
+
     except Exception as ex:
         print(ex)
     finally:
