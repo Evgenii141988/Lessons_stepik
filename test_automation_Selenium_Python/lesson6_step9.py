07ae16ebed5b0fe17e251e564cb115ab3df454cb
Evgenii <evgenii889111@gmail.com>
2022-05-16 19:50:56 +0300

    add link creat object brawser lesson6_step9

--- test_automation_Selenium_Python/lesson6_step9.py
+++ test_automation_Selenium_Python/lesson6_step9.py
@@ -3,5 +3,8 @@ from selenium.webdriver.common.by import By
 import time
 
 if __name__ == '__main__':
+    link = 'http://suninjuly.github.io/registration1.html'
+    brawser = webdriver.Chrome()
+    brawser.get(link)
 
 
