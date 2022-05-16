0a5a69c9aca2eb85a5dcff24d08d4edc2e30d63c
Evgenii <evgenii889111@gmail.com>
2022-05-16 18:20:43 +0300

    add try/finally lesson6_step8

--- test_automation_Selenium_Python/lesson6_step8.py
+++ test_automation_Selenium_Python/lesson6_step8.py
@@ -3,4 +3,12 @@ from selenium.webdriver.common.by import By
 import time
 
 if __name__ == '__main__':
-    pass
+    link = 'http://suninjuly.github.io/find_xpath_form'
+    try:
+        browser = webdriver.Chrome()
+        browser.get(link)
+    except Exception as ex:
+        print(ex)
+    finally:
+        time.sleep(30)
+        browser.quit()
