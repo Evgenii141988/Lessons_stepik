5e60747cf76b25e825f966472803debcbba1253d
Evgenii <evgenii889111@gmail.com>
2022-05-16 16:15:09 +0300

    add from selenium.webdriver.common.by import By   and change elements lesson6_step7

--- test_automation_Selenium_Python/lesson6_step7.py
+++ test_automation_Selenium_Python/lesson6_step7.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 try:
     browser = webdriver.Chrome()
     browser.get("http://suninjuly.github.io/huge_form.html")
-    elements = browser.find_elements_by_ * (value)
+    elements = browser.find_elements(By.TAG_NAME, 'input')
     for element in elements:
         element.send_keys("Мой ответ")
 
