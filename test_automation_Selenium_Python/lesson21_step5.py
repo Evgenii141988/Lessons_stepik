f27c0020c926ce68c139197f5371b4abee88e5f7
Evgenii <evgenii889111@gmail.com>
2022-05-17 18:02:41 +0300

    create function calc() lesson21_step5

--- test_automation_Selenium_Python/lesson21_step5.py
+++ test_automation_Selenium_Python/lesson21_step5.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import math
+
+
+def calc(x: int) -> str:
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
 
 if __name__ == '__main__':
-
+    pass
