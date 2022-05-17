ddaae64c1d381a64d94ab78cb482a853fc58e3cb
Evgenii <evgenii889111@gmail.com>
2022-05-17 19:20:28 +0300

    add time.sleep

--- test_automation_Selenium_Python/lesson21_step5.py
+++ test_automation_Selenium_Python/lesson21_step5.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import math
-
+import time
 
 def calc(x: str) -> str:
     return str(math.log(abs(12 * math.sin(int(x)))))
@@ -20,4 +20,5 @@ if __name__ == '__main__':
     except Exception as ex:
         print(ex)
     finally:
+        time.sleep(10)
         browser.quit()
