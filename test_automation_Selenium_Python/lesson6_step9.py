492d6b14b2ec88f2c037b8e43c072f8b88238fbd
Evgenii <evgenii889111@gmail.com>
2022-05-17 10:59:10 +0300

    pep8 lesson6_step9

--- test_automation_Selenium_Python/lesson6_step9.py
+++ test_automation_Selenium_Python/lesson6_step9.py
@@ -4,16 +4,16 @@ import time
 
 if __name__ == '__main__':
     link = 'http://suninjuly.github.io/registration1.html'
-    brawser = webdriver.Chrome()
-    brawser.get(link)
+    browser = webdriver.Chrome()
+    browser.get(link)
     try:
-        input_first_name = brawser.find_element(By.CSS_SELECTOR, 'div.first_block .first')
+        input_first_name = browser.find_element(By.CSS_SELECTOR, 'div.first_block .first')
         input_first_name.send_keys('Ivan')
-        input_last_name = brawser.find_element(By.CSS_SELECTOR, 'div.first_block .second')
+        input_last_name = browser.find_element(By.CSS_SELECTOR, 'div.first_block .second')
         input_last_name.send_keys('Ivanov')
-        input_email = brawser.find_element(By.CSS_SELECTOR, 'div.first_block .third')
+        input_email = browser.find_element(By.CSS_SELECTOR, 'div.first_block .third')
         input_email.send_keys('ivan_ivanov@mail.ru')
-        button = brawser.find_element(By.CSS_SELECTOR, 'button.btn')
+        button = browser.find_element(By.CSS_SELECTOR, 'button.btn')
         button.click()
 
         # Проверяем, что смогли зарегистрироваться
@@ -21,7 +21,7 @@ if __name__ == '__main__':
         time.sleep(1)
 
         # находим элемент, содержащий текст
-        welcome_text_elm = brawser.find_element(By.TAG_NAME, 'h1')
+        welcome_text_elm = browser.find_element(By.TAG_NAME, 'h1')
         # записываем в переменную welcome_text текст из элемента welcome_text_elt
         welcome_text = welcome_text_elm.text
 
@@ -29,4 +29,4 @@ if __name__ == '__main__':
         assert "Congratulations! You have successfully registered!" == welcome_text
     finally:
         time.sleep(10)
-        brawser.quit()
+        browser.quit()
