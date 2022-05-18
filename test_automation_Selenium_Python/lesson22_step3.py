4c970caa23b122f47d317b7429c3174ab4b6c18b
Evgenii <evgenii889111@gmail.com>
2022-05-18 16:35:40 +0300

    create browser, link

--- test_automation_Selenium_Python/lesson22_step3.py
+++ test_automation_Selenium_Python/lesson22_step3.py
@@ -6,6 +6,8 @@ import time
 
 if __name__ == '__main__':
     try:
-        pass
+        browser = webdriver.Chrome()
+        link = 'http://suninjuly.github.io/selects1.html'
+        browser.get(link)
     finally:
         time.sleep(10)
