36301f6322a69c51b5f143e0505397670dfeb7d2
Evgenii <evgenii889111@gmail.com>
2022-05-18 20:07:34 +0300

    browser, link

--- test_automation_Selenium_Python/lesson22_step8.py
+++ test_automation_Selenium_Python/lesson22_step8.py
@@ -6,6 +6,9 @@ import os
 
 if __name__ == '__main__':
     try:
-        pass
+        browser = webdriver.Chrome()
+        link = 'http://suninjuly.github.io/file_input.html'
+        browser.get(link)
+
     finally:
         time.sleep(10)
