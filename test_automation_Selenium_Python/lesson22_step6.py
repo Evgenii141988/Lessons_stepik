23176f2e2026887f9edd05c4ecaa4527f20c6503
Evgenii <evgenii889111@gmail.com>
2022-05-18 19:14:16 +0300

    create function calc

--- test_automation_Selenium_Python/lesson22_step6.py
+++ test_automation_Selenium_Python/lesson22_step6.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import math
 
+def calc(x: str) -> str:
+    return str(math.log(abs(12 * math.sin(int(x)))))
 
 if __name__ == '__main__':
     try:
         browser = webdriver.Chrome()
-        link = "https://SunInJuly.github.io/execute_script.html"
+        link = "http://suninjuly.github.io/execute_script.html"
         browser.get(link)
+
         button = browser.find_element(By.TAG_NAME, 'button')
         button.click()
     except Exception as ex:
