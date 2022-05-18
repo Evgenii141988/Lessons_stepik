2444cf14c183b911509c44ca8467f9b0ca94c389
Evgenii <evgenii889111@gmail.com>
2022-05-18 15:15:55 +0300

    create browser, link

--- test_automation_Selenium_Python/lesson21_step7.py
+++ test_automation_Selenium_Python/lesson21_step7.py
@@ -7,3 +7,6 @@ def calc(x: str) -> str:
     return str(math.log(abs(12 * math.sin(int(x)))))
 
 if __name__ == '__main__':
+    browser = webdriver.Chrome()
+    link = 'http://suninjuly.github.io/get_attribute.html'
+    browser.get(link)
