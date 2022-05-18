5f73e473a3356465c6da748346caaa5d85696638
Evgenii <evgenii889111@gmail.com>
2022-05-18 20:04:39 +0300

    create lesson22_step6

---
+++ test_automation_Selenium_Python/lesson22_step8.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+
+
+if __name__ == '__main__':
+    try:
+        pass
+    finally:
+        time.sleep(10)
