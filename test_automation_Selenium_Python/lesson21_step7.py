8115acf03627bc58c6cbb161be26605e1f87d376
Evgenii <evgenii889111@gmail.com>
2022-05-18 15:14:29 +0300

    create function culc()

--- test_automation_Selenium_Python/lesson21_step7.py
+++ test_automation_Selenium_Python/lesson21_step7.py
@@ -3,5 +3,7 @@ from selenium.webdriver.common.by import By
 import math
 import time
 
+def calc(x: str) -> str:
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
 if __name__ == '__main__':
-    pass
