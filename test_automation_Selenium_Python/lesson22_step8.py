fbc54458df2ed0bab63b57fa4560d7cb30c6441f
Evgenii <evgenii889111@gmail.com>
2022-05-18 20:09:34 +0300

    add input_first_name

--- test_automation_Selenium_Python/lesson22_step8.py
+++ test_automation_Selenium_Python/lesson22_step8.py
@@ -10,5 +10,7 @@ if __name__ == '__main__':
         link = 'http://suninjuly.github.io/file_input.html'
         browser.get(link)
 
+        input_first_name = browser.find_element(By.NAME, 'firstname')
+        input_first_name.send_keys('Ivan')
     finally:
         time.sleep(10)
