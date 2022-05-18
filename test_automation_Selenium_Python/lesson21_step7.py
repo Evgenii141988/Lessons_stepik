066aeb88ac813ce09679291d359f3e51a165bf99
Evgenii <evgenii889111@gmail.com>
2022-05-18 15:24:22 +0300

    add try/finally

--- test_automation_Selenium_Python/lesson21_step7.py
+++ test_automation_Selenium_Python/lesson21_step7.py
@@ -3,13 +3,19 @@ from selenium.webdriver.common.by import By
 import math
 import time
 
+
 def calc(x: str) -> str:
     return str(math.log(abs(12 * math.sin(int(x)))))
 
+
 if __name__ == '__main__':
-    browser = webdriver.Chrome()
-    link = 'http://suninjuly.github.io/get_attribute.html'
-    browser.get(link)
-    x_elem = browser.find_element(By.ID, 'treasure')
-    x = x_elem.get_attribute('valuex')
-    print(x)
+    try:
+        browser = webdriver.Chrome()
+        link = 'http://suninjuly.github.io/get_attribute.html'
+        browser.get(link)
+        x_elem = browser.find_element(By.ID, 'treasure')
+        x = x_elem.get_attribute('valuex')
+        print(type(x))
+    finally:
+        time.sleep(10)
+        browser.quit()
