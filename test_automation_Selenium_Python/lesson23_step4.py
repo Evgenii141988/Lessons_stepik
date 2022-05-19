13fba8acbb2d5a00f25b273e21ed099af53690c9
Evgenii <evgenii889111@gmail.com>
2022-05-19 14:16:44 +0300

    add confirm

--- test_automation_Selenium_Python/lesson23_step4.py
+++ test_automation_Selenium_Python/lesson23_step4.py
@@ -8,8 +8,13 @@ if __name__ == '__main__':
         browser = webdriver.Chrome()
         link = 'http://suninjuly.github.io/alert_accept.html'
         browser.get(link)
+
         button_one = browser.find_element(By.CSS_SELECTOR, 'button.btn')
         button_one.click()
+
+        confirm = browser.switch_to.alert
+        confirm.accept()
+
     finally:
         time.sleep(10)
         browser.quit()
