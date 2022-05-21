9959d9bad486657ae20b10d12751295d4206f515
Evgenii <evgenii889111@gmail.com>
2022-05-21 22:27:14 +0300

    create lessons32_step13

---
+++ test_automation_Selenium_Python/lesson32_step13.py
@@ -0,0 +1,46 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def input_data(link: str) -> str:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    try:
+        input_first_name = browser.find_element(By.CSS_SELECTOR, 'div.first_block .first')
+        input_first_name.send_keys('Ivan')
+        input_last_name = browser.find_element(By.CSS_SELECTOR, 'div.first_block .second')
+        input_last_name.send_keys('Ivanov')
+        input_email = browser.find_element(By.CSS_SELECTOR, 'div.first_block .third')
+        input_email.send_keys('ivan_ivanov@mail.ru')
+        button = browser.find_element(By.CSS_SELECTOR, 'button.btn')
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elm = browser.find_element(By.TAG_NAME, 'h1')
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elm.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        # assert "Congratulations! You have successfully registered!" == welcome_text
+    finally:
+        browser.quit()
+        return welcome_text
+
+class TestFormRegistration(unittest.TestCase):
+    def test_reg1(self):
+        self.assertEqual(input_data("http://suninjuly.github.io/registration1.html"),
+                         "Congratulations! You have successfully registered!")
+
+    def test_reg2(self):
+        self.assertEqual(input_data("http://suninjuly.github.io/registration2.html"),
+                         "Congratulations! You have successfully registered!")
+
+
+if __name__ == '__main__':
+    unittest.main()
