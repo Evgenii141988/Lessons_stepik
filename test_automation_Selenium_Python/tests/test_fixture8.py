c5b9d7a94ce286ce50a75a5bdcf94837372eff32
Evgenii <evgenii889111@gmail.com>
2022-05-23 15:55:24 +0300

    change test_fixture8.py and pytest.ini

--- test_automation_Selenium_Python/tests/test_fixture8.py
+++ test_automation_Selenium_Python/tests/test_fixture8.py
@@ -2,8 +2,8 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 link = "http://selenium1py.pythonanywhere.com/"
+
 
 @pytest.fixture(scope='function')
 def browser():
@@ -16,15 +16,22 @@ def browser():
 
 class TestMainPage1():
 
-    @pytest.mark.smoke
+    @pytest.mark.skip
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    @pytest.mark.regression
+    @pytest.mark.smoke
+    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "button.favorite")
+
 
 if __name__ == '__main__':
     pytest.main()
