63effdd088ac9d24734a95e331e7db58bf1c6455
Evgenii <evgenii889111@gmail.com>
2022-05-24 14:49:20 +0300

    create func browser as pytest.fixture, test_get_answer

--- test_automation_Selenium_Python/tests/test_lessons3_6_3.py
+++ test_automation_Selenium_Python/tests/test_lessons3_6_3.py
@@ -4,19 +4,36 @@ from selenium.webdriver.common.by import By
 import time
 import math
 
-@pytest.fixture(scope='Function')
+
+
+
+@pytest.fixture(scope='function')
 def browser():
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
 
+
 @pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1',
-                                    'https://stepik.org/lesson/236896/step/1',
-                                    'https://stepik.org/lesson/236897/step/1',
-                                    'https://stepik.org/lesson/236898/step/1',
-                                    'https://stepik.org/lesson/236899/step/1',
-                                    'https://stepik.org/lesson/236903/step/1',
-                                    'https://stepik.org/lesson/236904/step/1',
-                                    'https://stepik.org/lesson/236905/step/1'])
+                                  'https://stepik.org/lesson/236896/step/1',
+                                  'https://stepik.org/lesson/236897/step/1',
+                                  'https://stepik.org/lesson/236898/step/1',
+                                  'https://stepik.org/lesson/236899/step/1',
+                                  'https://stepik.org/lesson/236903/step/1',
+                                  'https://stepik.org/lesson/236904/step/1',
+                                  'https://stepik.org/lesson/236905/step/1'])
+# @pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1'])
 def test_get_answer(browser, link):
-    pass
+    browser.get(link)
+    browser.implicitly_wait(5)
+    input_answer = browser.find_element(By.CLASS_NAME, 'ember-text-area')
+    answer = math.log(int(time.time()))
+    input_answer.send_keys(answer)
+    button = browser.find_element(By.CSS_SELECTOR, 'button.submit-submission')
+    button.click()
+    browser.implicitly_wait(5)
+    elem_with_text = browser.find_element(By.CLASS_NAME, 'smart-hints__hint')
+    text_answer = elem_with_text.text
+    assert text_answer == "Correct!", 'Correct answer!!!'
+
+
