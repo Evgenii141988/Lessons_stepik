de80dee0e374e934c8f51e0400cc0ea043943d3c
Evgenii <evgenii889111@gmail.com>
2022-05-24 17:14:55 +0300

    correct code

--- test_automation_Selenium_Python/tests/test_lessons3_6_3.py
+++ test_automation_Selenium_Python/tests/test_lessons3_6_3.py
@@ -14,36 +14,38 @@ def browser():
     driver.quit()
 
 
-# @pytest.fixture(scope="function")
-# def answer():
-#     return math.log(int(time.time()))
+final_answer = []
 
-# @pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1',
-#                                   'https://stepik.org/lesson/236896/step/1',
-#                                   'https://stepik.org/lesson/236897/step/1',
-#                                   'https://stepik.org/lesson/236898/step/1',
-#                                   'https://stepik.org/lesson/236899/step/1',
-#                                   'https://stepik.org/lesson/236903/step/1',
-#                                   'https://stepik.org/lesson/236904/step/1',
-#                                   'https://stepik.org/lesson/236905/step/1'])
-@pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1'])
+
+@pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1',
+                                  'https://stepik.org/lesson/236896/step/1',
+                                  'https://stepik.org/lesson/236897/step/1',
+                                  'https://stepik.org/lesson/236898/step/1',
+                                  'https://stepik.org/lesson/236899/step/1',
+                                  'https://stepik.org/lesson/236903/step/1',
+                                  'https://stepik.org/lesson/236904/step/1',
+                                  'https://stepik.org/lesson/236905/step/1'])
 def test_get_answer(browser, link):
+    browser.get(link)
+    answer = math.log(int(time.time()))
+
+    input_answer = WebDriverWait(browser, 12).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.ember-text-area')))
+    input_answer.send_keys(answer)
+
+    button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.submit-submission')))
+    button.click()
+
+    elem_with_text = WebDriverWait(browser, 12).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint')))
+    text_answer = elem_with_text.text
     try:
-        browser.get(link)
-        answer = math.log(int(time.time()))
-        input_answer = WebDriverWait(browser, 12).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.ember-text-area')))
-        input_answer.send_keys(answer)
-        button = WebDriverWait(browser, 5).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.submit-submission')))
-        button.click()
-        browser.implicitly_wait(10)
-        elem_with_text = WebDriverWait(browser, 12).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint')))
-        text_answer = elem_with_text.text
-        assert text_answer == "Correct!", f'{text_answer}'
-    except AssertionError as error:
-        print(error.args[0])
+        assert text_answer == "Correct!", f'Here is the code: {text_answer}'
+    except AssertionError:
+        final_answer.append(text_answer)
+    finally:
+        print(*final_answer, sep='')
 
 
 if __name__ == '__main__':
