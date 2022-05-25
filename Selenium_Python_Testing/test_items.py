e31a40d910043b533d9339f01a40710badeda63a
Evgenii <evgenii889111@gmail.com>
2022-05-25 20:59:08 +0300

    Реализован тест test_check_button_add_to_basket, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину.

---
+++ Selenium_Python_Testing/test_items.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_button_add_to_basket(browser):
+    browser.get(link)
+    time.sleep(10)
+    button_add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    name_button = button_add_to_basket.text
+    assert name_button == 'Añadir al carrito', 'Language is not español'
