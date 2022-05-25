8b2f4ea0b1312777cfaff848925d054259ac1674
Evgenii <evgenii889111@gmail.com>
2022-05-25 21:44:07 +0300

    del name_button, change assert add is_enabled

--- Selenium_Python_Testing/test_items.py
+++ Selenium_Python_Testing/test_items.py
@@ -7,8 +7,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_check_button_add_to_basket(browser):
     browser.get(link)
     button_add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-    name_button = button_add_to_basket.text
-    assert name_button == 'Añadir al carrito', 'Choose language is not español'
+    assert button_add_to_basket.is_enabled(), 'WARNING!!! button add to basket is not found'
 
 
 if __name__ == '__main__':
