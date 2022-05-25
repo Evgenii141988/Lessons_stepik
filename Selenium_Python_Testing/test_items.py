85052356d58e42e167a711b386f90a88f062e918
Evgenii <evgenii889111@gmail.com>
2022-05-25 21:00:52 +0300

    add if __name__ with pytest.main

--- Selenium_Python_Testing/test_items.py
+++ Selenium_Python_Testing/test_items.py
@@ -11,3 +11,6 @@ def test_check_button_add_to_basket(browser):
     button_add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     name_button = button_add_to_basket.text
     assert name_button == 'Añadir al carrito', 'Language is not español'
+
+if __name__ == '__main__':
+    pytest.main()
