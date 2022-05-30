e4a4626b2879f7d8dee728506dd701283afaf901
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-30 13:50:23 +0300

    rename var

--- Python_basic_tests_and_tasks/task_2_1_11.py
+++ Python_basic_tests_and_tasks/task_2_1_11.py
@@ -3,6 +3,6 @@ def get_body_mass_index(m: float, h: float) -> float:
 
 
 if __name__ == '__main__':
-    M = float(input())
-    H = float(input())
-    print(get_body_mass_index(M, H))
+    weight = float(input())
+    height = float(input())
+    print(get_body_mass_index(weight, height))
