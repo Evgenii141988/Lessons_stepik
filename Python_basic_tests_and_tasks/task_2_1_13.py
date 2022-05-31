6049058704139408b10d52da3f0e134585da36b4
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-31 14:28:53 +0300

    Новая задача task_2_1_13, где реализована функция нахождения самого большого простого числа из списка чисел

---
+++ Python_basic_tests_and_tasks/task_2_1_13.py
@@ -0,0 +1,21 @@
+# На вход программе подается список целых чисел, разделённых через пробел.
+# Напишите программу, которая найдет среди них м
+# и выведет на экран индекс числа в списке и через пробел сумму его цифр.
+
+def get_largest_simple_num(numbers: list) -> int:
+    largest_simple_num = 0
+    counter = 0
+    for num in numbers:
+        for i in range(2, num // 2 + 1):
+            if num % i == 0:
+                counter += 1
+            if counter == 1:
+                counter = 0
+                break
+        if counter == 0 and num > largest_simple_num:
+            largest_simple_num = num
+    return largest_simple_num
+
+
+if __name__ == '__main__':
+    pass
