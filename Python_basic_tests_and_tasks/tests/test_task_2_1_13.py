bbf3a0bbd07568e6cfac6aa29ce841157573f24b
Evgenii Zalesski <evgenii889111@gmail.com>
2022-05-31 14:54:17 +0300

    тест для проверки функции для нахождения индекса простого числа

--- Python_basic_tests_and_tasks/tests/test_task_2_1_13.py
+++ Python_basic_tests_and_tasks/tests/test_task_2_1_13.py
@@ -1,6 +1,6 @@
 import pytest
 
-from Python_basic_tests_and_tasks.task_2_1_13 import get_largest_simple_num
+from Python_basic_tests_and_tasks.task_2_1_13 import get_largest_simple_num, get_index_num
 
 
 @pytest.mark.parametrize('numbers, expected_result', [([1, 2, 3, 5, 19], 19),
@@ -11,6 +11,16 @@ from Python_basic_tests_and_tasks.task_2_1_13 import get_largest_simple_num
                                                       ])
 def test_func_get_largest_simple_num(numbers, expected_result):
     assert get_largest_simple_num(numbers) == expected_result
+
+
+@pytest.mark.parametrize('numbers, expected_result', [([1, 2, 3, 5, 19], 4),
+                                                      ([3, 11, 7, 17, 6, 4, 19], 6),
+                                                      ([15, 4, 6, 9, 13, 89, 97, 25, 35, 124, 146], 6),
+                                                      ([2, 3, 5, 7, 13, 11, 37, 61, 71, 83, 79], 9),
+                                                      ([4, 6, 9, 15, 22, 44, 45, 35, 124, 146], -1),
+                                                      ])
+def test_func_get_index_num(numbers, expected_result):
+    assert get_index_num(numbers) == expected_result
 
 
 if __name__ == '__main__':
