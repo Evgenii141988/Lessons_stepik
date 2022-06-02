f29af5012f7d23a472103124881fc504c6301551
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-02 13:10:54 +0300

    тест функции get_squad_numbers

--- Python_basic_tests_and_tasks/tests/test_task_2_1_15.py
+++ Python_basic_tests_and_tasks/tests/test_task_2_1_15.py
@@ -1,6 +1,6 @@
 import pytest
 
-from Python_basic_tests_and_tasks.task_2_1_15 import get_round_numbers
+from Python_basic_tests_and_tasks.task_2_1_15 import get_round_numbers, get_squad_numbers
 
 
 @pytest.mark.parametrize('numbers, expected_result', [([2.1, 3.5, 2.9, 3.1], [3, 4, 3, 4]),
@@ -10,6 +10,15 @@ from Python_basic_tests_and_tasks.task_2_1_15 import get_round_numbers
                                                       ([1, 2, 3], [1, 2, 3])])
 def test_func_get_round_numbers(numbers, expected_result):
     assert get_round_numbers(numbers) == expected_result
+
+
+@pytest.mark.parametrize('numbers, expected_result', [([2, 3, 2, 3], [4, 9, 4, 9]),
+                                                      ([1, 24, 35], [1, 576, 1225]),
+                                                      ([3, 10, 1, 100], [9, 100, 1, 10000]),
+                                                      ([0, 0, 0], [0, 0, 0]),
+                                                      ([1, 2, 3], [1, 4, 9])])
+def test_func_get_squad_numbers(numbers, expected_result):
+    assert get_squad_numbers(numbers) == expected_result
 
 
 if __name__ == '__main__':
